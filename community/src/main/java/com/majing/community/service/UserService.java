11554aa8298830343c88478f0e9a925d13e29ea8
majing <majing350@gmail.com>
2023-09-12 19:35:27 +0800

    开发注册功能

--- community/src/main/java/com/majing/community/service/UserService.java
+++ community/src/main/java/com/majing/community/service/UserService.java
@@ -2,6 +2,8 @@ package com.majing.community.service;
 
 import com.majing.community.entity.User;
 
+import java.util.Map;
+
 /**
  * @author majing
  * @date 2023-08-02 20:52
@@ -11,8 +13,23 @@ public interface UserService {
     /**
      * 根据id获取用户
      * @param id
-     * @return com.majing.community.entity.User
+     * @return User
      * @created at 2023/8/2 20:53
     */
     User getUserById(Integer id);
+    /**
+     * 用户注册功能
+     * @param user
+     * @return java.lang.String
+     * @created at 2023/9/12 15:23
+    */
+    Map<String, Object> register(User user);
+    /**
+     * 实现激活的业务功能
+     * @param userId
+     * @param code
+     * @return java.lang.Integer
+     * @created at 2023/9/12 17:21
+    */
+    Integer activation(Integer userId, String code);
 }
--- community/src/main/java/com/majing/community/service/impl/UserServiceImpl.java
+++ community/src/main/java/com/majing/community/service/impl/UserServiceImpl.java
@@ -3,8 +3,20 @@ package com.majing.community.service.impl;
 import com.majing.community.dao.UserMapper;
 import com.majing.community.entity.User;
 import com.majing.community.service.UserService;
+import com.majing.community.util.CommunityUtil;
+import com.majing.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author majing
@@ -14,12 +26,91 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
+    private final MailClient mailClient;
+    private final TemplateEngine templateEngine;
+    @Value("${community.path.domain}")
+    private String domain;
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
     @Autowired
-    public UserServiceImpl(UserMapper userMapper) {
+    public UserServiceImpl(UserMapper userMapper, MailClient mailClient, TemplateEngine templateEngine) {
         this.userMapper = userMapper;
+        this.mailClient = mailClient;
+        this.templateEngine = templateEngine;
     }
     @Override
     public User getUserById(Integer id) {
         return userMapper.selectById(id);
+    }
+
+    @Override
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
+        //空值处理
+        if(user == null){
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+        if(StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMessage", "用户名不能为空");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getPassword())){
+            map.put("passwordMessage", "密码不能为空");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getEmail())){
+            map.put("emailMessage", "邮箱不能为空");
+            return map;
+        }
+        //验证账号
+        User user1 = userMapper.selectByName(user.getUsername());
+        if(user1 != null){
+            map.put("usernameMessage", "用户名已经存在");
+            return map;
+        }
+        if(user.getPassword().length() < 8){
+            map.put("passwordMessage", "密码不能小于8位");
+            return map;
+        }
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(user.getPassword());
+        if(!matcher.matches()){
+            map.put("passwordMessage", "密码必须同时包含数字，大小写字母和特殊字符");
+            return map;
+        }
+        user1 = userMapper.selectByEmail(user.getEmail());
+        if(user1 != null){
+            map.put("emailMessage", "邮箱已被注册");
+            return map;
+        }
+        //注册用户
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("https://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        userMapper.insertUser(user);
+        //邮箱激活
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        String url = domain + contextPath + "/activation" + "/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        String content = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
+        return map;
+    }
+    public Integer activation(Integer userId, String code){
+        User user = userMapper.selectById(userId);
+        if(user.getStatus() == 1){
+            return 0;
+        }else if (user.getActivationCode().equals(code)){
+            userMapper.updateStatus(userId, 1);
+            return 1;
+        }else {
+            return 2;
+        }
+
     }
 }
